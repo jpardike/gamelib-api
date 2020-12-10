c22e23f8d9929faec30540bcce116bfc0f48157b
jpardike <jpardike@gmail.com>
2020-12-10 00:45:29 -0800

    Updates game controllers

--- controllers/games.js
+++ controllers/games.js
@@ -1,30 +1,80 @@
 const db = require("../models");
 
 const index = (req, res) => {
+  // db.Game.find({})
+  //   .then((foundGames) => {
+  //     res.json({ games: foundGames });
+  //   })
+  //   .catch((err) => {
+  //     console.log('Error in games.index: ', err);
+  //     res.json({ Error: 'Unable to get your data' });
+  //   });
+
+  //   console.log('underneath database query');
+
   db.Game.find({}, (err, foundGames) => {
     if (err) console.log("Error in games#index:", err);
 
-    res.send("Incomplete games#index controller function");
+    if (!foundGames.length) {
+      return res.json({ message: "No games in the database" });
+    }
+
+    res.json({ games: foundGames });
+
+    // res.send("Incomplete games#index controller function");
   });
 };
 
 const show = (req, res) => {
+  // db.Game.findById(req.params.id)
+  // .then((foundGame) => {
+  //   res.json({ game: foundGame })
+  // })
+  // .catch((err) => {
+  //   console.log('Error in games.show: ', err);
+  //   res.json({ Error: 'Unable to get data' });
+  // });
+
   db.Game.findById(req.params.id, (err, foundGame) => {
     if (err) console.log("Error in games#show:", err);
 
-    res.send("Incomplete games#show controller function");
+    res.json({ game: foundGame });
   });
 };
 
 const create = (req, res) => {
+  // db.Game.create(req.body)
+  //   .then((savedGame) => {
+  //     res.json({ game: savedGame })
+  //   })
+  //   .catch((err) => {
+  //     console.log('Error in post games.index: ', err);
+  //     res.json({ Error: 'Unable to get data' });
+  //   })
+
   db.Game.create(req.body, (err, savedGame) => {
     if (err) console.log("Error in games#create:", err);
 
-    res.send("Incomplete games#create controller function");
+    console.log(req.body);
+
+    res.json({ games: savedGame });
   });
 };
 
 const update = (req, res) => {
+  // db.Game.findByIdAndUpdate(
+  //   req.params.id,
+  //   req.body,
+  //   { new: true }
+  // )
+  //   .then((updatedGame) => {
+  //     res.json({ game: updatedGame });
+  //   })
+  //   .catch((err) => {
+  //     console.log('Error in put games.update: ', err);
+  //     res.json({ Error: 'Unable to get data' });
+  //   });
+
   db.Game.findByIdAndUpdate(
     req.params.id,
     req.body,
@@ -32,16 +82,25 @@ const update = (req, res) => {
     (err, updatedGame) => {
       if (err) console.log("Error in games#update:", err);
 
-      res.send("Incomplete games#update controller function");
+      res.json({ game: updatedGame });
     }
   );
 };
 
 const destroy = (req, res) => {
+  // db.Game.findByIdAndDelete(req.params.id)
+  //   .then((deletedGame) => {
+  //     res.json({ game: deletedGame });
+  //   })
+  //   .catch((err) => {
+  //     console.log('Error in put games.update: ', err);
+  //     res.json({ Error: 'Unable to get data' });
+  //   });
+
   db.Game.findByIdAndDelete(req.params.id, (err, deletedGame) => {
     if (err) console.log("Error in games#destroy:", err);
 
-    res.send("Incomplete games#destroy controller function");
+    res.json({ game: deletedGame });
   });
 };
 
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 // imports
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes");
 
 const port = process.env.PORT || 4000;
 const app = express();
 
+const corsOptions = {
+  origin: 'http://localhost:3000'
+}
+
 // middleware - JSON parsing
-// app.use(express.json());
+app.use(express.json());
+app.use(cors(corsOptions));
 
 // middleware - API routes
 app.use("/api/v1/games", routes.games);
